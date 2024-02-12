b1cda56fb9ba90603942a4e9f121ceb9bbe056e7
Ansh Goyal <anshgoyal1704@gmail.com>
2024-02-12 10:03:34 +0000

    add dynamic file versioning
    Signed-off-by: Ansh Goyal <anshgoyal1704@gmail.com>

--- src/decrypt.js
+++ src/decrypt.js
@@ -8,19 +8,21 @@ const readFileAsync = promisify(fs.readFile);
 
 const algorithm = 'aes-256-cbc'; // Using AES encryption algorithm
 
-function decryptFile(encryptedData, token) {
+async function decrypt(inputFilePath, token) {
+    const encryptedData = await readFileAsync(inputFilePath, 'utf8');
     const decipher = crypto.createDecipher(algorithm, token);
     let decryptedData = decipher.update(encryptedData, 'hex', 'utf8');
     decryptedData += decipher.final('utf8');
-    const base64Data = Buffer.from(decryptedData, 'base64');
-    return base64Data;
-}
-
-async function decrypt(inputFilePath, token) {
-    const encryptedData = await readFileAsync(inputFilePath, 'utf8');
-    const decryptedData = decryptFile(encryptedData, token);
-    const outputPath = path.join(path.dirname(inputFilePath), 'decrypted.txt');
-    await writeFileAsync(outputPath, decryptedData);
+    console.log(decryptedData)
+    const separatorIndex = decryptedData.indexOf('@');
+    if (separatorIndex === -1) {
+        throw new Error('Invalid encrypted file format');
+    }
+    const fileInfoString = decryptedData.slice(0, separatorIndex);
+    const base64Data = decryptedData.slice(separatorIndex + 1);
+    const fileInfo = JSON.parse(fileInfoString);
+    const outputPath = path.join(path.dirname(inputFilePath), 'decrypted_' + fileInfo.name);
+    await writeFileAsync(outputPath, Buffer.from(base64Data, 'base64'));
     console.log('File decrypted successfully:', outputPath);
 }
 
--- src/encrypt.js
+++ src/encrypt.js
@@ -4,21 +4,26 @@ const { promisify } = require('util');
 const crypto = require('crypto');
 
 const writeFileAsync = promisify(fs.writeFile);
+const statAsync = promisify(fs.stat);
 
 const algorithm = 'aes-256-cbc'; // Using AES encryption algorithm
 
-function encryptFile(inputFilePath, token) {
-    const inputFile = fs.readFileSync(inputFilePath);
-    const base64Data = Buffer.from(inputFile).toString('base64');
-    const cipher = crypto.createCipher(algorithm, token);
-    let encryptedData = cipher.update(base64Data, 'utf8', 'hex');
-    encryptedData += cipher.final('hex');
-    return encryptedData;
-}
-
 async function encrypt(inputFilePath, token) {
-    const encryptedData = encryptFile(inputFilePath, token);
-    const outputPath = path.join(path.dirname(inputFilePath), 'encrypted.txt');
+    const inputFile = fs.readFileSync(inputFilePath);
+    const fileStats = await statAsync(inputFilePath);
+    const fileInfo = {
+        name: path.basename(inputFilePath),
+        type: path.extname(inputFilePath).slice(1), // Get rid of the dot
+        size: fileStats.size
+    };
+    const fileInfoString = JSON.stringify(fileInfo);
+    console.log(fileInfoString)
+    const base64Data = Buffer.from(inputFile).toString('base64');
+    const dataToEncrypt = fileInfoString + '@' + base64Data; // Use ':' as a separator
+    const cipher = crypto.createCipher(algorithm, token);
+    let encryptedData = cipher.update(dataToEncrypt, 'utf8', 'hex');
+    encryptedData += cipher.final('hex');
+    const outputPath = path.join(path.dirname(inputFilePath), fileInfo.name + '.txt');
     await writeFileAsync(outputPath, encryptedData, 'utf8');
     console.log('File encrypted successfully:', outputPath);
 }
